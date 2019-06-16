440c59be999db69d4beebba100da669402556416
grodfor <vlad.sitsko@gmail.com>
2019-06-16 14:03:00 +0300

    add test for model

--- app/src/main/java/sitsko/vlad/epamandroidproject/util/ReadJsonFromFile.java
+++ app/src/main/java/sitsko/vlad/epamandroidproject/util/ReadJsonFromFile.java
@@ -1,4 +1,30 @@
 package sitsko.vlad.epamandroidproject.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
 public class ReadJsonFromFile {
+
+    public static String loadJSONFromAsset(InputStream is) {
+        String json = null;
+
+        try {
+            int size = is.available();
+
+            byte[] buffer = new byte[size];
+
+            is.read(buffer);
+
+            is.close();
+
+            json = new String(buffer, StandardCharsets.UTF_8);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+
+        return json;
+
+    }
 }
--- app/src/test/java/sitsko/vlad/epamandroidproject/TestModel.java
+++ app/src/test/java/sitsko/vlad/epamandroidproject/TestModel.java
@@ -4,42 +4,26 @@ import com.google.gson.Gson;
 
 import org.junit.Test;
 
+import java.io.InputStream;
+import java.util.Objects;
+
 import sitsko.vlad.epamandroidproject.model.BasicModel;
+import sitsko.vlad.epamandroidproject.util.ReadJsonFromFile;
 
 public class TestModel {
 
     @Test
-    public void test(){
-
-        String basicModelJson =
-                "{\"section\": \"automobiles\",\"num_results\": 2, \"results\": [\"section\": \"Business\",\"title\": \"Crash Scene Investigations, With Automakers on the Case\", \"abstract\": \"Special teams dispatched by a number of manufacturers glean valuable information on what happens to vehicles and their occupants after a crash.\",\"byline\": \"By TOM VOELK\",\"published_date\": \"2019-05-09T06:00:04-04:00\",\"multimedia\": [ {\"url\": \"https://static01.nyt.com/images/2019/05/09/business/09wheels-1/09wheels-1-thumbStandard.jpg\",\"format\": \"Standard Thumbnail\"}],\"short_url\": \"https://nyti.ms/2LykNf1\"}]}"
-                ;
-
-        String basicModelJson1 = "{\n" +
-                "  \"section\": \"automobiles\",\n" +
-                "  \"num_results\": 2,\n" +
-                "  \"results\": " +
-              //  "[\n" +
-                "    {\n" +
-                "      \"section\": \"Business\",\n" +
-                "      \"title\": \"Crash Scene Investigations, With Automakers on the Case\",\n" +
-                "      \"abstract\": \"Special teams dispatched by a number of manufacturers glean valuable information on what happens to vehicles and their occupants after a crash.\",\n" +
-                "      \"byline\": \"By TOM VOELK\",\n" +
-                "      \"published_date\": \"2019-05-09T06:00:04-04:00\",\n" +
-                "      \"multimedia\": [\n" +
-                "        {\n" +
-                "          \"url\": \"https://static01.nyt.com/images/2019/05/09/business/09wheels-1/09wheels-1-thumbStandard.jpg\",\n" +
-                "          \"format\": \"Standard Thumbnail\"\n" +
-                "        }\n" +
-                "      ],\n" +
-                "      \"short_url\": \"https://nyti.ms/2LykNf1\"\n" +
-                "    }\n" +
-               // "  ]\n" +
-                "}";
+    public void test() {
 
         Gson gson = new Gson();
 
-        BasicModel basicModel = gson.fromJson(basicModelJson1, BasicModel.class);
+        InputStream is = (this.getClass().getClassLoader()).getResourceAsStream("response.json");
+
+        String s = ReadJsonFromFile.loadJSONFromAsset(is);
+        System.out.println(s);
+
+        BasicModel basicModel = gson.fromJson(s, BasicModel.class);
+
         System.out.println("end");
     }
 }
